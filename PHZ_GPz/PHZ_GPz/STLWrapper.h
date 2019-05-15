403f97354150787823c236639df3a93ad2815989
Corentin Schreiber <cschreib@orange.fr>
2019-05-15 18:09:09 +0100

    Added parallel_for helper

--- PHZ_GPz/PHZ_GPz/STLWrapper.h
+++ PHZ_GPz/PHZ_GPz/STLWrapper.h
@@ -29,6 +29,9 @@
 #include <vector>
 #include <numeric>
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <cassert>
 
 namespace PHZ_GPz {
 
@@ -64,6 +67,121 @@ namespace PHZ_GPz {
             std::chrono::high_resolution_clock::now().time_since_epoch()
         ).count()*1e-6;
     }
+
+    struct parallel_for {
+        uint_t chunk_size = 0;
+
+    private :
+
+        struct worker_t {
+            std::unique_ptr<std::thread> thread;
+            parallel_for& pfor;
+
+            explicit worker_t(parallel_for& p) : pfor(p) {}
+
+            worker_t(worker_t&& w) : pfor(w.pfor) {}
+
+            worker_t(const worker_t& w) = delete;
+
+            template<typename F>
+            void start(const F& f) {
+                thread = std::unique_ptr<std::thread>(new std::thread([this,&f]() {
+                    uint_t i0, i1;
+                    while (pfor.query_chunk(i0, i1)) {
+                        for (uint_t i = i0; i < i1; ++i) {
+                            f(i);
+                        }
+                    }
+                }));
+            }
+
+            void join() {
+                thread->join();
+                thread = nullptr;
+            }
+        };
+
+        // Workers
+        std::vector<parallel_for::worker_t> workers;
+
+        // Internal
+        std::mutex query_mutex;
+        uint_t n, i0, i1, di;
+
+        bool query_chunk(uint_t& oi0, uint_t& oi1) {
+            std::unique_lock<std::mutex> l(query_mutex);
+
+            if (i0 == n) {
+                return false;
+            }
+
+            oi0 = i0;
+            oi1 = i1;
+
+            i0 = i1;
+            i1 += di;
+            if (i1 > n) {
+                i1 = n;
+            }
+
+            return true;
+        }
+
+    public :
+
+        parallel_for() = default;
+        parallel_for(const parallel_for&) = delete;
+        parallel_for(parallel_for&&) = delete;
+
+        explicit parallel_for(uint_t nthread) {
+            for (uint_t i = 0; i < nthread; ++i) {
+                workers.emplace_back(*this);
+            }
+        }
+
+        template<typename F>
+        void execute(const F& f, uint_t ifirst, uint_t ilast) {
+            assert(ilast >= ifirst && "loop index must be increasing");
+
+            n = ilast - ifirst;
+
+            if (workers.empty()) {
+                // Single-threaded execution
+                for (uint_t i = ifirst; i < ilast; ++i) {
+                    f(i);
+                }
+            } else {
+                // Multi-threaded execution
+                uint_t nchunk = (chunk_size == 0 ?
+                    workers.size() : std::max(workers.size(), n/chunk_size));
+
+                // Setup chunks
+                di = n/nchunk + 1;
+                i0 = ifirst;
+                i1 = ifirst + di;
+
+                // Launch threads
+                for (auto& t : workers) {
+                    t.start(f);
+                }
+
+                // Join all
+                for (auto& t : workers) {
+                    t.join();
+                }
+            }
+        }
+
+        template<typename F>
+        void execute(const F& f, uint_t i1) {
+            execute(f, 0, i1);
+        }
+
+        uint_t size() const {
+            return workers.size();
+        }
+    };
+
 
 }  // namespace PHZ_GPz
 
